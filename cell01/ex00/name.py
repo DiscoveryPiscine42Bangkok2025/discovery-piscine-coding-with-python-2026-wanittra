545b147d4ecc3f97351eac86d1229c3ed007c6c4
WANITTRA <wanittra.fern@gmail.com>
2026-02-10 23:21:55 +0700

    Add scripts for name and age display

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Wanittra"
+last_name = "Pitaksa"
+print(first_name, last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Wanittra"
+last_name = "Pitaksa"
+whole_name = first_name + " " + last_name
+print(whole_name)
